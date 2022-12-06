1ba95565fe7fb035d904502cce282e6c7d695f9a
Thai Huu Luong <102707239+THAIHUULUONG@users.noreply.github.com>
2022-12-06 15:49:14 +0700

    cleanr build

--- src/components/Pancake-uikit/components/Tag/Tag.tsx
+++ src/components/Pancake-uikit/components/Tag/Tag.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { scales, TagProps } from "./types";
+import {TagProps } from "./types";
 import { StyledTag } from "./StyledTag";
 
 const Tag: React.FC<TagProps> = ({ startIcon, endIcon, children, ...props }) => (
   <StyledTag {...props}>
     {React.isValidElement(startIcon) &&
       React.cloneElement(startIcon, {
-        mr: "0.5em",
       })}
     {children}
     {React.isValidElement(endIcon) &&
       React.cloneElement(endIcon, {
-        ml: "0.5em",
       })}
   </StyledTag>
 );
